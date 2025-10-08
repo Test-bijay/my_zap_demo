1af25712d6586160093b0ca5be021d24653e3680
Bijay Dahal <bijay@hitechnnepal.com.np>
2025-10-08 17:06:36 +0545

    files_are_changed

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -12,15 +12,20 @@ describe('Welcome to ZAP Test App', () => {
   });
 
   it('navigates to Test XSS and shows expected output', () => {
+    // If the app executes the payload and shows an alert:
+    cy.on('window:alert', (text) => {
+      expect(text).to.contain('XSS');
+    });
+
     cy.contains('Test XSS').click();
-    cy.url().should('include', '/xss');
-    cy.contains(/xss test/i).should('be.visible');
+    cy.url().should('include', '/xss?name=');
   });
 
   it('navigates to Test SQL Injection and shows expected output', () => {
+    cy.intercept('GET', '**/login**').as('loginAttempt');
     cy.contains('Test SQL Injection').click();
-    cy.url().should('include', '/sql-injection');
-    cy.contains(/sql injection/i).should('be.visible');
+    cy.wait('@loginAttempt').its('request.url').should('include', "user=%27%20OR%20%271%27=%271");
+    cy.url().should('include', '/login');
   });
 
   it('navigates to View Debug Info and shows expected output', () => {
