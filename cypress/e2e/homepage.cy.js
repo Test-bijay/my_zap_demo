b4f34cd9e44fceff574e1e407c1a41a73eb742de
Bijay Dahal <bijay@hitechnnepal.com.np>
2025-10-08 13:06:52 +0545

    cypress file edited

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,6 +1,13 @@
 describe('Welcome to ZAP Test App', () => {
   it('loads homepage and shows expected content', () => {
     cy.visit('/');
+
+    // Check the main heading
     cy.contains(/welcome to zap test app/i).should('be.visible');
+
+    // Check all expected links are visible
+    cy.contains('Test XSS').should('be.visible');
+    cy.contains('Test SQL Injection').should('be.visible');
+    cy.contains('View Debug Info').should('be.visible');
   });
 });
